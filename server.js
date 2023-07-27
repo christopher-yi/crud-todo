d1f58872d4902d42068589c6b448910fe7ca9436
christopher-yi <christopher.yi319@gmail.com>
2023-07-26 23:24:01 -0400

    Set up server root route and add models and views

--- server.js
+++ server.js
@@ -7,16 +7,26 @@ const app = express()
 const PORT = 8500
 const mongoose = require('mongoose')
 require('dotenv').config()
-// Add model variable
+const TodoTask = require('./models/todotask')
 
 // Set middleware
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 
-mongoose.connect(process.env.DB_CONNECTION, 
-    {useNewUrlParser: true},
-    () => {console.log('Connected to db!')}
-)
+mongoose.connect(process.env.DB_STRING, 
+    {useNewUrlParser: true})
+    .then(() => console.log('Connected to db!'))
+    .catch((err) => { console.error(err) })
+
+app.get('/', async (res, req) => {
+    try {
+        TodoTask.find({}, (err, tasks) => {
+            res.render('index.ejs', {todoTasks: tasks})
+        })
+    } catch (err) {
+        if (err) return res.statusCode(500).send(err)
+    }
+})
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
